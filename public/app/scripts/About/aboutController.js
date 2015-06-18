1b117d6aaeff4f6261fceb4719aedd738ecde04b
dustmouse <paul.palubinski@gmail.com>
2015-06-17 22:05:29 -0600

    More general cleanup.

--- public/app/scripts/About/aboutController.js
+++ public/app/scripts/About/aboutController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('brewApp.controllers')
-	.controller('AboutCtrl', [aboutController]);
+	.controller('AboutCtrl', aboutController);
 
 function aboutController() {
 	
-};
+}
