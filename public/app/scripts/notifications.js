f7e2d2fa1b371283d59e96fd70733abc352f5c08
Sam <phreakdawg@gmail.com>
2015-06-18 19:25:30 -0600

    Added other toastr methods to notification service.

--- public/app/scripts/notifications.js
+++ public/app/scripts/notifications.js
@@ -7,10 +7,25 @@
     var notifications = function() {
         var success = function(msg) {
             toastr.success(msg);
+        };
+
+        var info = function(msg) {
+          toastr.info(msg);
+        };
+
+        var warning = function(msg) {
+            toastr.warning(msg);
         }
 
+        var error = function(msg) {
+            toastr.error(msg);
+        };
+
         return {
-            success: success
+            success: success,
+            info: info,
+            warning: warning,
+            error: error
         };
     };
 
