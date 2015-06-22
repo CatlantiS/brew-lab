8b99dcf2c04e2076bebf5f1661e9bf1fc8e83617
dustmouse <paul.palubinski@gmail.com>
2015-06-21 22:40:41 -0600

    Added placeholder for other nav bar actions.

--- public/app/scripts/NavSidebar/navSidebarController.js
+++ public/app/scripts/NavSidebar/navSidebarController.js
@@ -1,15 +1,25 @@
 'use strict';
 
 angular.module('brewApp.controllers')
-	.controller('NavSidebarCtrl', navSidebarController);
+	.controller('NavSidebarCtrl', ['notifications', navSidebarController]);
 
-function navSidebarController() {
+function navSidebarController(notifications) {
 	/* jshint validthis: true */
 	var self = this;
 
 	self.actions = [{
 			title: 'Enter a new recipe',
 			url: '/recipe'
+		}, {
+			title: 'My recipes',
+			click: function() {
+				notifications.error('Eat a johnson.')
+			}
+		}, {
+			title: 'Saved recipes',
+			click: function() {
+				notifications.error('Farkled it up, Farkleton.')
+			}
 		}
 	]
 }
