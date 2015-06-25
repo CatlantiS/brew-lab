691cab47d668f9a49c2e049dd2b9628ebc94dc7f
Sam <phreakdawg@gmail.com>
2015-06-25 14:25:54 -0600

    Binds datatable and populates name field

--- public/app/scripts/UI/brewTable.js
+++ public/app/scripts/UI/brewTable.js
@@ -1,11 +1,27 @@
 'use strict';
 
+var flatterData = function(arr) {
+	var res = [];
+	var i;
+	for (i=0; i < arr.length; i++) {
+		res.push([arr[i].name, '', '', '']);
+	}
+	console.log('res = ');
+	console.log(res);
+	return res;
+}
+
 angular.module('brewApp.directives')
     .directive('brewTable', function() {
         return {
             restrict: 'AE',
             link: function(scope, element) {
-                $(element).DataTable(scope.dataTableOptions);
+            	scope.$watch('ctrl.isLoading', function() {
+            		if (!scope.ctrl.isLoading) {
+            			scope.dataTableOptions.data = flatterData(scope.ctrl.recipes);
+                		$(element).DataTable(scope.dataTableOptions);
+                	}
+            	});
             }
         };
     });
