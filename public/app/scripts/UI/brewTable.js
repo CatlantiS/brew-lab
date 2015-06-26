eacc4094f47e05a6eefa5826c29a772c81e4392a
dustmouse <paul.palubinski@gmail.com>
2015-06-25 23:42:04 -0600

    Added $q.when

--- public/app/scripts/UI/brewTable.js
+++ public/app/scripts/UI/brewTable.js
@@ -1,20 +1,13 @@
 'use strict';
 
 angular.module('brewApp.directives')
-    .directive('brewTable', function() {
+    .directive('brewTable', ['$q', function($q) {
         return {
             restrict: 'AE',
             link: function(scope, element) {
-                scope.loadData().then(function(options) {
+                $q.when(scope.loadData()).then(function(options) {
                     $(element).DataTable(options);
                 });
-
-            	//scope.$watch('ctrl.isLoading', function() {
-            	//	if (!scope.ctrl.isLoading) {
-            			//scope.ctrl.dataTableOptions.data = scope.ctrl.getDataForDataTable();
-                		//$(element).DataTable(scope.ctrl.dataTableOptions);
-                	//}
-            	//});
             }
         };
-    });
+    }]);
