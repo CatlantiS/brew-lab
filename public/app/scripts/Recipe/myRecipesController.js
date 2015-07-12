0ef45cde4303168f6fed587558c7a9ef620cfb66
dustmouse <paul.palubinski@gmail.com>
2015-07-12 17:38:50 -0600

    Creating pointless version lib.

--- public/app/scripts/Recipe/myRecipesController.js
+++ public/app/scripts/Recipe/myRecipesController.js
@@ -1,15 +1,25 @@
 'use strict';
 
 angular.module('brewApp.controllers')
-    .controller('MyRecipesCtrl', ['$scope', 'Recipe', myRecipesController]);
+    .controller('MyRecipesCtrl', ['$scope', 'Recipe', 'Version', myRecipesController]);
 
-function myRecipesController($scope, Recipe) {
+function myRecipesController($scope, Recipe, Version) {
     /* jshint validthis: true */
     var self = this;
 
     $scope.loadData = function() {
-
         return Recipe.getRecipes().then(function(recipes) {
+            var recipe = new Version(recipes[0]);
+
+            recipe.name = 'you blew it';
+
+            delete recipe.name;
+
+            alert(recipe.name);
+
+            recipe.deleteProp('name');
+
+            alert(recipe.name);
 
             return options = {
                 data: recipes.map(function(recipe) {
