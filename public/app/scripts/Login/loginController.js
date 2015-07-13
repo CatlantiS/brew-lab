f0ab930d2291ea44709a7934f879b7bfd99c6ee6
Sam <phreakdawg@gmail.com>
2015-07-13 16:58:40 -0600

    pointing login at server method

--- public/app/scripts/Login/loginController.js
+++ public/app/scripts/Login/loginController.js
@@ -2,21 +2,29 @@
 
 (function() {
     angular.module('brewApp.controllers')
-        .controller('LoginCtrl', ['$scope', 'notifications', LoginController]);
+        .controller('LoginCtrl', ['$scope', '$http','notifications', LoginController]);
 
-    function LoginController($scope, notifications) {
+    function LoginController($scope, $http, notifications) {
         $scope.isLogin = true;
         var login = {}
 
         login.ProcessLogin = function() {
             console.log(login);
-            if (login.username == 'brewuser' && login.password == 'meow') {
+            /*if (login.username == 'brewuser' && login.password == 'meow') {
                 notifications.success('login successful!');
             }
             else
             {
                 notifications.error('Bad username and/or password.');
-            }
+            }*/
+
+            $http.post('/login', {username: login.username, password: login.password})
+                .success(function(data) {
+                    notifications.info('Login successful');
+                })
+                .error(function(data,status) {
+                    notifications.error('Login failed, ' + status);
+                });
         }
 
         $scope.login = login;
