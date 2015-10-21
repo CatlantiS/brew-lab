1d011c82d49dc79e3f01e2d1f1f3bd5ac717c231
dustmouse <paul.palubinski@gmail.com>
2015-10-20 19:56:52 -0600

    Added joinPaths helper method.

--- public/app/scripts/Common/helper.js
+++ public/app/scripts/Common/helper.js
@@ -28,11 +28,24 @@
             return '<a class="row-delete" href="javascript:void(0);" data-id="' + dataId + '" title="Delete"><i class="fa fa-trash brew-delete"></i></a>';
         }
 
+        //Probably a nicer way to do this.
+        function joinPaths(paths) {
+            if (arguments.length > 1) paths = Array.prototype.slice.call(arguments);
+
+            return paths.reduce(function(base, route) {
+                if (base[base.length - 1] === '/' && route[0] === '/')
+                    base = base.substring(0, base.length - 1);
+
+                return base + route;
+            });
+        }
+
         return {
             deResourcify: deResourcify,
             deResourcifyArray: deResourcifyArray,
             getRowEditIconHtml: getRowEditIconHtml,
-            getRowDeleteIconHtml: getRowDeleteIconHtml
+            getRowDeleteIconHtml: getRowDeleteIconHtml,
+            joinPaths: joinPaths
         };
     };
 })();
--- public/app/scripts/Configuration/configuration.js
+++ public/app/scripts/Configuration/configuration.js
@@ -2,14 +2,13 @@
 (function() {
     'use strict';
 
-    angular.module('brewApp.services').factory('Configuration', configuration);
+    angular.module('brewApp.services').factory('Configuration', ['Helper', configuration]);
 
-    function configuration() {
+    function configuration(Helper) {
         //This can be expanded.
         function Url(base) { this.base = base; return this; }
 
-        //Can make this much cooler.
-        Url.prototype.route = function(name, route) { this[name] = this.base + route; return this; };
+        Url.prototype.route = function(name, route) { this[name] = Helper.joinPaths(this.base, route); return this; };
 
         var storeUrl = new Url('http://localhost:3000').route('api', '/api/v1/'),
             authUrl = new Url('http://localhost:3000/authorize');
