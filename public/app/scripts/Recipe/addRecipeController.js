854c2bd563795de49c35a3938de47b3a067314b7
dustmouse <paul.palubinski@gmail.com>
2015-12-23 13:17:28 -0700

    Added other ingredients to add recipe.  Cleaned some stuff up.

--- public/app/scripts/Recipe/addRecipeController.js
+++ public/app/scripts/Recipe/addRecipeController.js
@@ -7,6 +7,8 @@ function addRecipeController($scope, AppState, BrewMaster, notifications, logger
     /* jshint validthis: true */
     var self = this;
 
+    self.INGREDIENT_TYPE = { HOPS: 'hops', MALT: 'malt', YEAST: 'yeast' };
+
     //Might not need to worry about AppState, just depends on how the UI will look.
     self.recipe = AppState.area('AddRecipe').recipe || {};
 
@@ -24,16 +26,43 @@ function addRecipeController($scope, AppState, BrewMaster, notifications, logger
         self.current[type] = null;
     };
 
-    self.deleteIngredient = function(type, ingredient, identifier) {
+    self.deleteIngredient = function(type, ingredient) {
         var index = self.recipe[type]
-            .map(function(i) { return i[identifier]; })
-            .indexOf(ingredient[identifier]);
+            .map(function(i) { return i.name; })
+            .indexOf(ingredient.name);
 
         self.recipe[type].splice(index, 1);
     };
 
     self.submit = function(isValid) {
         if (isValid) {
+            self.recipe.ingredients = self.recipe.ingredients || [];
+
+            //A lot of redundancy in here.
+            if (self.recipe.hops && self.recipe.hops.length > 0)
+                for (var i = 0; i < self.recipe.hops.length; i++) {
+                    var hops = self.recipe.hops[i];
+                    hops.type = self.INGREDIENT_TYPE.HOPS;
+
+                    self.recipe.ingredients.push(hops);
+                }
+
+            if (self.recipe.malt && self.recipe.malt.length > 0)
+                for (i = 0; i < self.recipe.malt.length; i++) {
+                    var malt = self.recipe.malt[i];
+                    malt.type = self.INGREDIENT_TYPE.MALT;
+
+                    self.recipe.ingredients.push(malt);
+                }
+
+            if (self.recipe.yeast && self.recipe.yeast.length > 0)
+                for (i = 0; i < self.recipe.yeast.length; i++) {
+                    var yeast = self.recipe.yeast[i];
+                    yeast.type = self.INGREDIENT_TYPE.YEAST;
+
+                    self.recipe.ingredients.push(yeast);
+                }
+
             //Need a spinner on this?
             UserStore.saveRecipe(self.recipe).then(function(data) {
                 notifications.success('Recipe ' + self.recipe.name + ' saved.');
