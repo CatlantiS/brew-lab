4606010acfb9e137e9a645e75422b0051477a6ba
dustmouse <paul.palubinski@gmail.com>
2015-12-31 22:36:16 -0700

    Cleaned up edit recipe controller.

--- public/app/scripts/Recipe/editRecipeController.js
+++ public/app/scripts/Recipe/editRecipeController.js
@@ -1,91 +1,80 @@
-//Could've used the same controller for add and edit, but figured it would be cleaner to keep them separate even though there is redundancy.
+//Can refactor to eliminate redundancy between this and the add recipe controller.
+(function() {
+    'use strict';
 
-'use strict';
+    angular.module('brewApp.controllers')
+        .controller('EditRecipeCtrl', ['$modalInstance', 'BrewMaster', 'logger', 'notifications', 'UserStore', '$modalParams', editRecipeController]);
 
-angular.module('brewApp.controllers')
-    .controller('EditRecipeCtrl', ['$modalInstance', 'BrewMaster', 'logger', 'notifications', 'UserStore', '$modalParams', editRecipeController]);
+    function editRecipeController($modalInstance, BrewMaster, logger, notifications, UserStore, $modalParams) {
+        /* jshint validthis: true */
+        var self = this;
 
-function editRecipeController($modalInstance, BrewMaster, logger, notifications, UserStore, $modalParams) {
-    /* jshint validthis: true */
-    var self = this, original = {};
+        self.INGREDIENT_TYPE = { HOPS: 'hops', MALT: 'malt', YEAST: 'yeast' };
 
-    self.INGREDIENT_TYPE = { HOPS: 'hops', MALT: 'malt', YEAST: 'yeast' };
+        UserStore.getCurrentUserRecipeById($modalParams.id).then(function(recipe) {
+            self.recipe = angular.copy(recipe);
 
-    UserStore.getCurrentUserRecipeById($modalParams.id).then(function(recipe) {
-        self.recipe = angular.copy(recipe);
+            UserStore.getRecipeIngredientsByRecipeId($modalParams.id).then(function(ingredients) {
+                //Break out ingredients by type for UI.
+                if (ingredients && ingredients.length > 0) {
+                    self.ingredients = [];
 
-        UserStore.getRecipeIngredientsByRecipeId($modalParams.id).then(function(ingredients) {
-            //Break out ingredients by type for UI.
-            if (ingredients && ingredients.length > 0) {
-                self.ingredients = [];
-
-                for (var i = 0; i < ingredients.length; i++)
-                    (self.ingredients[ingredients[i].type] = self.ingredients[ingredients[i].type] || []).push(ingredients[i]);
-            }
-        });
-    });
-
-    BrewMaster.getDefinitions().then(function(definitions) {
-        self.units = definitions.units.volume.map(function(def) { return def.name; });
-
-        if (BrewMaster.hasIngredient(definitions, 'yeast'))
-            self.yeasts = definitions.ingredient.yeast.map(function(def) { return def.name; });
-    });
-
-    self.addIngredient = function(type, ingredient) {
-        (self.ingredients[type] = self.ingredients[type] || []).push(ingredient);
-
-        //This is pretty brittle.
-        self.current[type] = null;
-    };
-
-    self.deleteIngredient = function(type, ingredient) {
-        var index = self.ingredients[type]
-            .map(function(i) { return i.name; })
-            .indexOf(ingredient.name);
-
-        self.ingredients[type].splice(index, 1);
-    };
-
-    self.update = function(isValid) {
-        if (isValid) {
-            self.recipe.ingredients = self.recipe.ingredients || [];
-
-            for (var key in self.INGREDIENT_TYPE) {
-                if (!self.INGREDIENT_TYPE.hasOwnProperty(key)) continue;
-
-                var type = self.INGREDIENT_TYPE[key];
-
-                if (self.ingredients[type] && self.ingredients[type].length > 0)
-                    for (var i = 0; i < self.ingredients[type].length; i++) {
-                        var ingredient = self.ingredients[type][i];
-                        ingredient.type = type;
-
-                        self.recipe.ingredients.push(ingredient);
-                    }
-            }
-
-            //Need a spinner on this?
-            UserStore.updateRecipe(self.recipe).then(function() {
-                notifications.success('Recipe ' + self.recipe.name + ' updated.');
-                logger.info('Recipe ' + self.recipe.recipeId + ' updated.');
-
-                clearForm();
-
-                $modalInstance.close();
+                    for (var i = 0; i < ingredients.length; i++)
+                        (self.ingredients[ingredients[i].type] = self.ingredients[ingredients[i].type] || []).push(ingredients[i]);
+                }
             });
-        }
-    };
+        });
 
-    self.cancel = function() {
-        clearForm();
+        BrewMaster.getDefinitions().then(function(definitions) {
+            self.units = definitions.units.volume.map(function(def) { return def.name; });
 
-        $modalInstance.dismiss();
-    };
+            if (BrewMaster.hasIngredient(definitions, 'yeast'))
+                self.yeasts = definitions.ingredient.yeast.map(function(def) { return def.name; });
+        });
 
-    function clearForm() {
-        self.recipeForm.$setPristine();
-        self.recipe = {};
-        self.ingredients = [];
+        self.addIngredient = function(type, ingredient) {
+            (self.ingredients[type] = self.ingredients[type] || []).push(ingredient);
+
+            //This is pretty brittle.
+            self.current[type] = null;
+        };
+
+        self.deleteIngredient = function(type, ingredient) {
+            var index = self.ingredients[type]
+                .map(function(i) { return i.name; })
+                .indexOf(ingredient.name);
+
+            self.ingredients[type].splice(index, 1);
+        };
+
+        self.update = function(isValid) {
+            if (isValid) {
+                self.recipe.ingredients = self.recipe.ingredients || [];
+
+                for (var key in self.INGREDIENT_TYPE) {
+                    if (!self.INGREDIENT_TYPE.hasOwnProperty(key)) continue;
+
+                    var type = self.INGREDIENT_TYPE[key];
+
+                    if (self.ingredients[type] && self.ingredients[type].length > 0)
+                        for (var i = 0; i < self.ingredients[type].length; i++) {
+                            var ingredient = self.ingredients[type][i];
+                            ingredient.type = type;
+
+                            self.recipe.ingredients.push(ingredient);
+                        }
+                }
+
+                //Need a spinner on this?
+                UserStore.updateRecipe(self.recipe).then(function() {
+                    notifications.success('Recipe ' + self.recipe.name + ' updated.');
+                    logger.info('Recipe ' + self.recipe.recipeId + ' updated.');
+
+                    $modalInstance.close();
+                });
+            }
+        };
+
+        self.cancel = function() { $modalInstance.dismiss(); };
     }
-}
+})();
